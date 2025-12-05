e71fb6f9d1599c0259dd8bd190795bc3f62232b7
Mehmet Kara <mkara200546@gmail.com>
2025-12-05 12:02:10 -0500

    updated mac address dictionary

--- Other/Tools/Butterworth Results/MACID_to_PC_TYPE.m
+++ Other/Tools/Butterworth Results/MACID_to_PC_TYPE.m
@@ -1,9 +1,12 @@
 %% 4C-D7-17-97-99-A6 = Dell Precision 3460 (Scott E200 computers)
     % Intel core i7-14700
-    % 32GB ram
+    % RAM: 32GB DDR4 5600 MT/s
+    % Average performance: 2708 ops/sec
 %% 6C-2B-59-DC-40-27 = Dell Precision unknown (PACE/Cube computers)
-    % Intel core i7 - unknown
-    % unknown ram
+    % CPU: Intel core i7 7800X
+    % RAM: 128 GB DDR4 4800 MT/s
+    % Average performance: 1061 ops/sec
 %% A4-83-E7-17-AD-70 = 2018 Macbook Pro (Mehmet Kara's laptop)
-    % Intel core i7
-    % 16GB ram 8860H
+    % CPU: Intel core i7 8860H
+    % RAM: 16GB DDR4 4800 MT/s
+    % Average performance: 756 ops/sec
