38215876083bdd16c70f863e9f3295f1dd690157
Mehmet Kara <mkara200546@gmail.com>
2025-12-05 12:18:22 -0500

    e225 testing

--- Other/Tools/Butterworth Results/MACID_to_PC_TYPE.m
+++ Other/Tools/Butterworth Results/MACID_to_PC_TYPE.m
@@ -10,3 +10,9 @@
     % CPU: Intel core i7 8860H
     % RAM: 16GB DDR4 4800 MT/s
     % Average performance: 756 ops/sec
+
+%% 18-60-24-A5-77-36 = Dell Z4 (E225 computers)
+    % CPU: Intel Xeon W2123 (4 cores @ 3.6Ghz)
+    % RAM: 32GB DDR4 2666 MT/s
+    % GPU: NVIDIA Quadro P2000
+    % Average performance: 1006 ops/sec
