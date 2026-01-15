20a3ea04c254aa3aa6fdc5f8c72a1611d4387ecb
Mehmet Kara <mkara200546@gmail.com>
2026-01-15 00:26:52 -0500

    Created CarSim data truncation tool

---
+++ Battery Modeling/Tools/CarSimoutput_to_moduletesting.m
@@ -0,0 +1,31 @@
+%% Description:
+
+% This is a tool to convert a CarSim output .CSV file
+% It truncates the file so that only the relevant coloumns/signals are used
+% This one specifically only keeps current, voltage, temp, SoC and time!
+    % Meant for module testing and comparison!
+
+
+%% Code start
+
+% Initialization
+
+clc; clear all; close all;
+
+% Define the cols to keep
+
+colsToKeep = [1, 819, 64, 712, 751];
+
+% Load the .CSV
+
+carsimdata = readtable("carsimdata");
+
+% Truncate the data
+outputdata = carsimdata(:, colsToKeep);
+
+% Write the data to a CSV output
+writetable(outputdata, 'outputdata.csv');
+
+fprintf('File saved successfully as outputdata.csv\n');
+
+
